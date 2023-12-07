1e42cca9b4f24d4e398017044c3ab89d77875d42
Louai Dourov <92324961+Louai-Zokerburg@users.noreply.github.com>
2023-12-06 17:59:17 -0800

    removing top creators

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,8 +1,8 @@
 import { Models } from "appwrite";
 
 // import { useToast } from "@/components/ui/use-toast";
-import { Loader, PostCard, UserCard } from "@/components/shared";
-import { useGetRecentPosts, useGetUsers } from "@/lib/react-query/queries";
+import { Loader, PostCard } from "@/components/shared";
+import { useGetRecentPosts } from "@/lib/react-query/queries";
 
 const Home = () => {
   // const { toast } = useToast();
@@ -12,13 +12,8 @@ const Home = () => {
     isLoading: isPostLoading,
     isError: isErrorPosts,
   } = useGetRecentPosts();
-  const {
-    data: creators,
-    isLoading: isUserLoading,
-    isError: isErrorCreators,
-  } = useGetUsers(10);
 
-  if (isErrorPosts || isErrorCreators) {
+  if (isErrorPosts) {
     return (
       <div className="flex flex-1">
         <div className="home-container">
@@ -50,20 +45,20 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="home-creators">
-        <h3 className="h3-bold text-light-1">Top Creators</h3>
-        {isUserLoading && !creators ? (
-          <Loader />
-        ) : (
-          <ul className="grid 2xl:grid-cols-2 gap-6">
-            {creators?.documents.map((creator) => (
-              <li key={creator?.$id}>
-                <UserCard user={creator} />
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+                {/* <div className="home-creators">
+                  <h3 className="h3-bold">Top Creators</h3>
+                  {isUserLoading && !creators ? (
+                    <Loader />
+                  ) : (
+                    <ul className="grid 2xl:grid-cols-2 gap-6">
+                      {creators?.documents.map((creator) => (
+                        <li key={creator?.$id}>
+                          <UserCard user={creator} />
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div> */}
     </div>
   );
 };
